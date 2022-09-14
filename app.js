7cecf64e5db5468c0ea4557e8bc6a9a29809a75a
Lukas Larsson Røtting <htmlukas@gmail.com>
2022-09-14 13:05:45 +0200

    js funskjon for sticky header
    La til en javascript-fil som linkes til html-filen. Den skal gjøre headeren sticky, slik at den henger med nedover siden når man scroller.

---
+++ app.js
@@ -0,0 +1,17 @@
+// Når brukeren scroller, utføres myFunction
+window.onscroll = function() {myFunction()};
+
+// Henter elementet header
+var header = document.getElementById("header");
+
+// Henter posisjonen som skal utlignes av headeren
+var sticky = header.offsetTop;
+
+// Legger til klassen sticky til headeren når man scroller
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
